92c0a8acc0283f0dd38c5e2c87fd57ff58d2102a
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-11-20 11:57:39 +0530

    feat: refining prompts

--- localServer/constants/systemPrompt.py
+++ localServer/constants/systemPrompt.py
@@ -2,6 +2,9 @@ OUTPUT_FORMAT = '''
 
 You should output an array of object where each object handles one input. The object describes the input field and what value should be there.
 
+For text input fied directly give selector for input field.
+If there is a radio button, give selector for that and keep handleType as 'choice'\
+
 [
   // Input field one
   {
@@ -11,11 +14,11 @@ You should output an array of object where each object handles one input. The ob
   },
   {
   {
-    "handleType" : Literal["text", "dropdown", "checkbox", "choice"]
+    "handleType" : Literal["text", "choice"]
     "value" : str,
     "selector" : str // give the string that i will put inside document.queryselector()
   },
-    "handleType" : Literal["text", "dropdown", "checkbox", "choice"]
+    "handleType" : Literal["text", "choice"]
     "value" : str,
     "selector" : str // give the string that i will put inside document.queryselector()
   },
@@ -25,7 +28,12 @@ You should output an array of object where each object handles one input. The ob
 
 SYSTEM_PROMPT = f'''
 
-You wil be given a HTML documennt. It is a form. You need to fill out those part of forms which have text as input. You will be given releavant context for the same.
+You wil be given a HTML documennt. It is a form. 
+You need to fill out those part of forms which have text or radio as input. 
+You will be given releavant context for the same.
+Only fill those details for which information is available.
+If you are using any data from FAQ Please don't change any words from that. Use that as it is.  
+
 
 This should be your output format - 
 {OUTPUT_FORMAT}
