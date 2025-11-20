8c24769f2aa217810a4ed6912220eeeb7712f88c
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-11-20 12:20:47 +0530

    refactor: of prompts

---
+++ chromeExtension/src/constants/faq.js
@@ -0,0 +1,27 @@
+export const FAQ = `
+Q: Describe anything you built end-to-end.
+
+A: You will surely enjoy reading this blog where I nerd about what I built - 
+https://www.khushalsindhav.com/blog/read/kubernetes-cluster-i-shouldnt-have-built
+Github : https://github.com/MagnusCarlsen26/Practo-Scraper
+
+Q: Give an example of you learning a new technology quickly.
+
+A: Learning new technologies has always come naturally to me. For example, I picked up React.js without knowing JavaScript beforehand, leveraging the syntactic similarities with Python. I watched React.js tutorials for nine hours straight and jumped right into building projects, troubleshooting errors through persistent Googling.
+
+During my summer internship, I had to use Firebase as the sole developer on the project. I sought help from ChatGPT (before there was Cursor), asking "How to do CRUD operations in Firebase?" and then adapted the solutions into my codebase.
+
+I'm comfortable learning from documentation or by asking AI for relevant information, using my own reasoning to quickly grasp new concepts.
+
+This ability is thanks in part to the discipline and experience I gained preparing for JEE Advanced 2021.
+
+
+Q. Why should you hire me?
+
+A. 1) Strong Problem solving skills - Proved by being in top 0.8% in JEE ADVANCE 2021.
+   2) Even as a junior SWE, I won't need a specialised document telling me each sub steps I need to complete. A high level description is enough for me - I will figure out rest of the details and a plan on how to convert idea to software.
+   3) Love for building cool stuff - I love building cool solutions with software. You pay me 5 LPA or 50 LPA you will get the same quality work from me.
+   4) I have previously worked at a early stage startup ( team of 5 ).  I know how small teams function.
+Company - https://in.linkedin.com/company/oasis-of-ideas
+Founder - https://www.linkedin.com/in/yuvraj-sarda ( Cornell University )
+`
